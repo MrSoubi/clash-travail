4eb56d87bd5ca6cb912cf51887311b69a1034a22
Mr Bisou <emmanuel.bouis@proton.me>
2025-10-25 13:48:49 +0200

    intégration sons mail challenge

--- Scripts/auto_mail.gd
+++ Scripts/auto_mail.gd
@@ -2,7 +2,6 @@ extends RichTextLabel
 
 @export var typing_sfx : AudioStreamPlayer
 @export var finished_sfx : AudioStreamPlayer
-@export var pitch_scale_range : float = 0.2
 
 signal on_finished
 
@@ -10,9 +9,8 @@ func _ready() -> void:
 	visible_characters = 0
 
 func _input(event: InputEvent) -> void:
-	if event is InputEventKey:
+	if Input.is_action_pressed("key_mail") and not event.is_echo():
 		visible_characters += 1
-		typing_sfx.pitch_scale = 1 + randf_range(-pitch_scale_range, pitch_scale_range)
 		typing_sfx.play()
 	
 	if visible_ratio == 1:
