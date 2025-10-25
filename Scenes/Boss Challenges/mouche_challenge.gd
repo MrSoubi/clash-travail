bfeed4a2ef0507bdad93b6b81996840d0080f642
Baptoast <perrin.baptiste78@gmail.com>
2025-10-25 16:44:53 +0200

    Mouche finished

--- Scenes/Boss Challenges/mouche_challenge.gd
+++ Scenes/Boss Challenges/mouche_challenge.gd
@@ -1,1 +1,15 @@
 extends Control
+
+@export var flies : Array[Node2D]
+
+var flies_count : int
+
+func _ready() -> void:
+	flies_count = flies.size()
+	for fly in flies:
+		fly.tree_exited.connect(on_fly_dead)
+
+func on_fly_dead():
+	flies_count -= 1
+	if flies_count == 0:
+		print("FINI")
--- Scenes/fly.gd
+++ Scenes/fly.gd
@@ -13,5 +13,6 @@ func _physics_process(delta: float) -> void:
 
 func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 	if event is InputEventMouseButton:
+		self.queue_free()
 		print("clicked")
 	pass
