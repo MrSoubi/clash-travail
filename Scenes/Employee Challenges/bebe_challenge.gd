da50db0775ccf68685daf21308d74d381df17be1
Baptoast <perrin.baptiste78@gmail.com>
2025-10-25 17:15:29 +0200

    bercer bébé comme un enculé terminé

---
+++ Scenes/Employee Challenges/bebe_challenge.gd
@@ -0,0 +1,67 @@
+extends Control
+
+@export var upSprite : Sprite2D
+@export var downSprite : Sprite2D
+var baseSize : Vector2 = Vector2(1, 1)
+var bigSize : Vector2 = Vector2(1.2, 1.2)
+
+var sequence = [
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+	'Up',
+	'Down',
+]
+
+var sequence_index = 0
+
+func _ready() -> void:
+	downSprite.scale = baseSize
+	downSprite.modulate.a = 0.5
+	upSprite.scale = bigSize
+	upSprite.modulate.a = 1
+
+func _input(event):
+	if event is InputEventKey and event.pressed:
+		if event.as_text_keycode() == sequence[sequence_index]:
+			sequence_index += 1
+			if event.as_text_keycode() == "Down":
+				downSprite.scale = baseSize
+				downSprite.modulate.a = 0.5
+				upSprite.scale = bigSize
+				upSprite.modulate.a = 1
+			if event.as_text_keycode() == "Up":
+				downSprite.scale = bigSize
+				downSprite.modulate.a = 1
+				upSprite.scale = baseSize
+				upSprite.modulate.a = 0.5
+			if sequence_index == sequence.size():
+				print("FINI")
+				sequence_index = 0
+		#else:
+			#sequence_index = 0
