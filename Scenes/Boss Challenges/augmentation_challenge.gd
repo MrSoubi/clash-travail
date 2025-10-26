6a8f50ab60d95792b8ea061e85386640b8703cac
Baptoast <perrin.baptiste78@gmail.com>
2025-10-26 15:18:55 +0100

    slider audio integration

--- Scenes/Boss Challenges/augmentation_challenge.gd
+++ Scenes/Boss Challenges/augmentation_challenge.gd
@@ -1,15 +1,34 @@
 extends Control
 
 @export var sliders : Array[Node]
+@export var dragAudioLoop : AudioStreamPlayer
+@export var valueOKAudio : AudioStreamPlayer
+@export var valueWrongAudio : AudioStreamPlayer
+@export var gameFinishedAudio : AudioStreamPlayer
 
 func _ready() -> void:
 	for s in sliders:
 		s.on_value_ok.connect(check_challenge)
+		s.on_value_wrong.connect(play_value_wrong)
+		s.on_interact.connect(play_sound)
+		s.on_interact_end.connect(stop_sound)
+
+func play_sound():
+	dragAudioLoop.play()
+
+func stop_sound():
+	dragAudioLoop.stop()
+	
+func play_value_wrong():
+	valueWrongAudio.play()
 
 func check_challenge():
 	var result : bool = true
+	valueOKAudio.play()
 	for s in sliders:
 		result = result and s.is_value_ok
 	
 	if result:
+		gameFinishedAudio.reparent(get_tree().root)
+		gameFinishedAudio.play()
 		EventBus.on_challenge_completed.emit()
--- Scenes/Boss Challenges/augmentation_slider.gd
+++ Scenes/Boss Challenges/augmentation_slider.gd
@@ -1,6 +1,9 @@
 extends VBoxContainer
 
 signal on_value_ok
+signal on_value_wrong
+signal on_interact
+signal on_interact_end
 
 @export var min_value : float
 @export var max_value : float
@@ -12,6 +15,11 @@ var is_value_ok : bool
 func _ready() -> void:
 	is_value_ok = false
 	slider.value_changed.connect(on_slider_changed)
+	slider.drag_started.connect(on_slider_dragged)
+	slider.drag_ended.connect(on_slider_drag_end)
+
+func on_slider_dragged():
+	on_interact.emit()
 
 func on_slider_changed(value : float):
 	if value <= max_value and value >= min_value:
@@ -21,3 +29,8 @@ func on_slider_changed(value : float):
 	else:
 		if is_value_ok:
 			is_value_ok = false
+			
+func on_slider_drag_end(value : bool):
+	if is_value_ok == false:
+		on_value_wrong.emit()
+	on_interact_end.emit()
