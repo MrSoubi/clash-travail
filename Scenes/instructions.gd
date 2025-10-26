a7bdc7ef9278e71544105dd52826863b32176aad
Mr Bisou <emmanuel.bouis@proton.me>
2025-10-26 09:32:02 +0100

    prep instructions

--- Scenes/instructions.gd
+++ Scenes/instructions.gd
@@ -1,10 +1,15 @@
 extends Control
 
 @export var animator : AnimationPlayer
-@export var prompt : Label
 
+@export var instruction_text : String
+@export var title_text : String
+@export var instruction_label : Label
+@export var title_label : Label
+@export var is_boss_challenge : bool
+
+# Utiliser le bool is_boss_challenge pour changer la position des instructions
 func _ready() -> void:
-	print("launched")
 	animator.play("Appear")
 
 func _on_timeout() -> void:
